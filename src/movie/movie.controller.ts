1f07a67fce916485f3ed34a185e83cba7f93f1e5
Angel Pantoja <angelpan17@hotmail.com>
2024-07-24 10:04:46 -0600

    Added swagger example

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get} from '@nestjs/common';
 import { MovieService } from './movie.service';
+import {
+  ApiResponse
+} from '@nestjs/swagger';
+import { Movie } from '../entities/movies.entity'
 
 @Controller()
 export class MovieController {
   constructor(private readonly MovieService: MovieService) {}
 
   @Get('movies')
-  getMovies(): string {
+  @ApiResponse({
+    status: 200,
+    description: 'The found movies',
+    type: Movie,
+  })
+  getMovies(): any {
     return this.MovieService.getMovies();
   }
 }
